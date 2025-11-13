5e1008364eb342511dfa66076adf5051c43c3726
manavos <anavos@calpoly.edu>
2025-11-13 12:21:29 -0800

    change to milestone 3 folder

---
+++ bootcamp-milestone-3/src/app/layout.tsx
@@ -0,0 +1,28 @@
+import type { Metadata } from "next"; // Object to set metadata
+
+import "./globals.css";
+import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
+
+
+export const metadata: Metadata = {
+  title: "Ashley Navos",
+  description: "A personal website for Ashley Navos.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+		// returns boilerplate
+  return (
+    <html lang="en">
+      <body className={`layout`}>
+        <Navbar />
+        <main className="content">{children}</main>
+        <Footer />
+      </body>
+    </html>
+  );
+}
