317314c263ec0dd90b198321665ec47e0d3c0dc6
manavos <155997183+manavos@users.noreply.github.com>
2025-11-13 17:44:32 -0800

    Delete bootcamp-milestone-3 directory

--- bootcamp-milestone-3/src/components/Footer.tsx
+++
@@ -1,8 +0,0 @@
-import style from "./footer.module.css";
-
-export default function Footer() {
-  return (
-   <footer className={style.footer}>© 2025 Ashley Navos | All Rights Reserved</footer>
-
-  );
-}
